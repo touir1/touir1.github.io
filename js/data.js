76d0da1abb355a47d99e6c6f872ec0b6bfa00476
touir1 <touir.mat@gmail.com>
2018-08-20 15:58:20 +0100

    updated Skills part

--- js/data.js
+++ js/data.js
@@ -3,6 +3,12 @@ var data = new function() {
     
     this.fr = {
         'domicile': '06 Rue Kawafel, 2040 Rades, Ben Arous, Tunisia',
+        'otherSkills': [
+            'Développement en C & structures de données de base du C++',
+            'Développement en utililisant le Spring framework (Java)',
+            'Développement Agile & Scrum',
+            'SQL, Mysql, Oracle(PL/SQL) & Sybase(TransactSQL)',
+        ],
     };
     this.en = {
         'name': 'Touir',
@@ -14,6 +20,46 @@ var data = new function() {
         'github': 'https://github.com/touir1',
         'twitter': 'https://twitter.com/TouirMohamedAli',
         'domicile': '06 Kawafel Street, 2040 Rades, Ben Arous, Tunisie',
+        'skills': [
+            {
+                'title': 'javascript', 
+                'iconClass': 'fab fa-js-square',
+            },
+            {
+                'title': 'python', 
+                'iconClass': 'fab fa-python',
+            },
+            {
+                'title': 'html5', 
+                'iconClass': 'fab fa-html5',
+            },
+            {
+                'title': 'css3', 
+                'iconClass': 'fab fa-css3-alt',
+            },
+            {
+                'title': 'java', 
+                'iconClass': 'fab fa-java',
+            },
+            {
+                'title': 'angularjs', 
+                'iconClass': 'fab fa-angular',
+            },
+            {
+                'title': 'nodejs', 
+                'iconClass': 'fab fa-node-js',
+            },
+            {
+                'title': 'npm', 
+                'iconClass': 'fab fa-npm',
+            },
+        ],
+        'otherSkills': [
+            'C Development & C++ basic data structures',
+            'Spring Java framework Development',
+            'Agile Development & Scrum',
+            'SQL, Mysql, Oracle(PL/SQL) & Sybase(TransactSQL)',
+        ],
     };
 
     this.default = this.en;
--- js/index.js
+++ js/index.js
@@ -2,13 +2,44 @@
 $("#user_surname").html(data.default.surname);
 $("#user_name").html(data.default.name);
 $('#user_fullName').html(data.default.surname + ' ' + data.default.name);
-$("#user_domicile").html(data.default.domicile);
 $("#user_email").html(data.default.email);
 $("#user_email").attr("href", "mailto:"+data.default.email);
 $("#user_phone").html(data.default.phone);
+$("#user_domicile").html(data.default.domicile);
 
 // initialize the social data
 $("#user_linkedin").attr("href", data.default.linkedin);
 $("#user_github").attr("href", data.default.github);
 $("#user_facebook").attr("href", data.default.facebook);
 $("#user_twitter").attr("href", data.default.twitter);
+
+// initialize the skills data
+// clear skills div before loop
+$("#user_skills").empty();
+// skills loop
+data.default.skills.forEach(function(item){
+    //append item in div
+    $("#user_skills").append(
+        "\
+        <li class=\"list-inline-item\">\
+        <i class=\""+item.iconClass+"\" title=\""+item.title+"\"></i>\
+        </li>\
+    ");
+});
+
+
+// initialize the other skills data
+// clear other skills div before loop
+$("#user_otherSkills").empty();
+// other skills loop
+data.default.otherSkills.forEach(function(item){
+    //append item in div
+    $("#user_otherSkills").append(
+        "\
+        <li>\
+        <i class=\"fa-li fa fa-check\"></i>\
+        "+item+"\
+        </li>\
+    ");
+});
+
